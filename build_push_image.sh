c2aa1908c423b0b4ca444d31db348623ef1d8ef0
satupercobaan12 <satupercobaan12@gmail.com>
2023-06-23 20:48:16 +0700

    update build_push_image.sh

--- build_push_image.sh
+++ build_push_image.sh
@@ -8,11 +8,11 @@ docker build -t item-app:v1 .
 # melihat seluruh daftar image di lokal
 docker images
 
-# Mengubah nama image agar sesuai dengan format github packages
+# Mengubah nama image agar sesuai dengan format dockerhub
 docker image tag item-app:v1 ghcr.io/swildz/item-app:v1
 
 # login ke github packages
-echo $PASSWORD_GITHUB_CONTAINER_REGISTRY | docker login ghcr.io -u swildz --password-stdin
+docker login ghcr.io -u swildz -p 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
 
 # push atau mengunggah image ke github packages
 docker push ghcr.io/swildz/item-app:v1
